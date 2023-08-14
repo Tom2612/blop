6291b4e5cce3a282208aeb486ffd56ec4258ddde
tom <tomcat-26@hotmail.co.uk>
2023-08-14 13:20:09 +0100

    Added more card controls

---
+++ src/components/Cards.jsx
@@ -0,0 +1,11 @@
+export default function Cards(props) {
+  return (
+    <div>
+        <label>Card count:</label>
+        <select name='cardCount' onChange={props.handleCardChange}>
+        {[...Array(props.maxCards)].map((e, i) => <option value={i + 1} key={i}>{i + 1}</option>)}
+        </select>
+        <h1>MaxCardCount: {props.maxCards}</h1>
+    </div>
+  )
+}
--- src/components/SetUpForm.jsx
+++ src/components/SetUpForm.jsx
@@ -1,21 +1,44 @@
 import { useState } from 'react';
 import Players from './Players';
+import Cards from './Cards';
 
 export default function SetUpForm() {
     const [players, setPlayers] = useState([]);
     const [name, setName] = useState('');
+    const [cardStart, setCardStart] = useState(false);
+    const [maxCards, setMaxCards] = useState(52);
+    const [cardCount, setCardCount] = useState(null);
 
     const handleChangeName = (e) => {
-        setName(e.target.value);
+      setName(e.target.value);
     }
 
     const handleSubmitName = () => {
-        setPlayers(prev => [...prev, name]);
-        setName('');
+      setPlayers(prev => [...prev, name]);
+      setName('');
+      // setMaxCards(findMaxCards(players.length));
+      console.log(findMaxCards(players.length))
     }
 
     const handleRemoveName = (e) => {
-        setPlayers(prev  => prev.filter(name => name !== e.target.textContent));
+      setPlayers(prev  => prev.filter(name => name !== e.target.textContent));
+    }
+
+    const handleSubmitPlayers = () => {
+      setCardStart(prev => !prev);
+      setMaxCards(findMaxCards(players.length));
+    }
+
+    const handleCardChange = () => {
+
+    }
+
+    const findMaxCards = (players) => {
+      for (let i = 52; i > 0; i--) {
+        if (i * players <= 52) {
+          return i;
+        }
+      }
     }
 
     return (
@@ -33,7 +56,13 @@ export default function SetUpForm() {
                 return <li key={'player' + i} onClick={handleRemoveName}>{ player }</li>
             })}
           </ul>
-          <label>Card count:</label>
+          <button onClick={handleSubmitPlayers}>Submit Players</button>
+          {cardStart && <Cards handleCardChange={handleCardChange} maxCards={maxCards} />}
+          {/* <label>Card count:</label>
+          <select name='cardCount'>
+            {[...Array(maxCards)].map((e, i) => <option value={i + 1} key={i}>{i + 1}</option>)}
+          </select>
+          <h1>MaxCardCount: {maxCards}</h1> */}
           <input type='number'></input>
           <button className='btn btn-success'>Start Game</button>
       </div>
